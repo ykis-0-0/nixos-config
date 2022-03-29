0677b1a3fa331ea3be689e4170914b357d07e382
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-03-30 00:51:30 +0800

    Make it more like a proper home

--- home.nix
+++ home.nix
@@ -1,22 +1,48 @@
 { config, pkgs, ... }:
-
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "nixos";
-  home.homeDirectory = "/home/nixos";
+  imports = let
+    vscode-srv = fetchGit {
+      url = "https://github.com/santicalcagno/nixos-vscode-server.git"; # "https://github.com/msteen/nixos-vscode-server.git";
+      ref = "master";
+    };
+  in [ "${vscode-srv}/modules/vscode-server/home.nix" ];
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "22.05";
+  home = {
+    # Home Manager needs a bit of information about you and the
+    # paths it should manage.
+    username = "nixos";
+    homeDirectory = "/home/nixos";
 
-  home.packages = with pkgs; [ asciinema nix-index ];
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "22.05";
+
+    enableNixpkgsReleaseCheck = true;
+
+    packages = with pkgs; [
+      nix-index nvd nix-tree
+      jq yq-go
+      asciinema deno
+      # taskwarrior
+      # broot eww
+      # dogdns
+    ];
+
+    # Useless unless we manage shell here lol
+    shellAliases = {
+      hm-install = "";
+    };
+  };
+
+  services = {
+    vscode-server.enable = true;
+  };
 
   programs = {
     # Let Home Manager install and manager itself.
@@ -31,6 +57,10 @@
 
       aliases = {
         graph = "log --graph '--pretty=tformat:%C(auto)%h%C(dim white)% ci%C(auto)% G?%d%n  [%cN]:% s'";
+      };
+
+      extraConfig = {
+        credential.helper = "store";
       };
     };
   };
