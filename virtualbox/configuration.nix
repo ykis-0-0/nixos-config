f58ed6d1f504f5f06e4f659d9cddeb1f531b7cba
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-03-30 01:28:40 +0800

    Seems I forgot to give it a hostname

--- virtualbox/configuration.nix
+++ virtualbox/configuration.nix
@@ -13,6 +13,7 @@
   nixpkgs.config.allowUnfree = true;
 
   networking = {
+    hostName = "vbox";
     # The global useDHCP flag is deprecated, therefore explicitly set to false here.
     # Per-interface useDHCP will be mandatory in the future, so this generated config
     # replicates the default behaviour.
