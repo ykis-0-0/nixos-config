25153639b23c48bee6dacee900e2e7484672249e
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-03-29 22:18:28 +0800

    More incorporation from `nixos-generate-config`

--- virtualbox/hardware.nix
+++ virtualbox/hardware.nix
@@ -1,14 +1,32 @@
 {
-  boot.loader = {
-    grub = {
-      enable = true;
-      device = "/dev/sda";
+  boot = {
+    kernelModules = [];
+    initrd = {
+      availableKernelModules = [ "ata_piix" "ochi_pci" "ehci_pci" "sd_mod" "sr_mod" ];
+      kernelModules = [];
+    };
+    extraModulePackages = [];
+
+
+    loader = {
+      efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        version = 2;
+        device = "/dev/sda";
+        efiSupport = true;
+      };
+      # systemd-boot.enable = true;
     };
   };
 
   virtualisation.virtualbox.guest.enable = true;
 
+  swapDevices = [];
+
   hardware = {
+    enableRedistributableFirmware = true;
+    # cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
     pulseaudio.enable = true;
   };
 
