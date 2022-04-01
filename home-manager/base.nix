0c1534b0158fdb5419363e23e1f97db25713cdca
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-04-02 04:47:48 +0800

    Split home configurations to allow composition

---
+++ home-manager/base.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+{
+  nixpkgs.config.allowUnfree = true;
+
+  home = {
+    enableNixpkgsReleaseCheck = true;
+
+    packages = with pkgs; [
+      nvd nix-diff nix-tree nix-index
+    ];
+  };
+}
