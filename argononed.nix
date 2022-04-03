80aae4e95eb4ae2e34b8db5f613299db4ea91cb2
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-04-03 18:36:41 +0800

    Add argononed tester config

---
+++ argononed.nix
@@ -0,0 +1,14 @@
+{ argononed, ...}:
+{
+  imports = [ "${argononed}/OS/nixos" ];
+
+  services.argonone = {
+    enable = true;
+    settings = {
+      fanTemp0 = 36; fanSpeed0 = 10;
+      fanTemp1 = 41; fanSpeed1 = 50;
+      fanTemp2 = 46; fanSpeed2 = 80;
+      hysteresis = 4;
+    };
+  };
+}
