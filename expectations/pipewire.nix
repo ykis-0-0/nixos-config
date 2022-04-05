ac6877f2aba125039586a80d5aa58a3894852560
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-04-05 15:42:02 +0800

    What if we outright disable both ALSA and PulseAudio?

--- expectations/pipewire.nix
+++ expectations/pipewire.nix
@@ -6,9 +6,9 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+    # alsa.enable = true;
+    # alsa.support32Bit = true;
+    # pulse.enable = true;
     config.pipewire = {
       "context.properties" = {
         #"link.max-buffers" = 64;
