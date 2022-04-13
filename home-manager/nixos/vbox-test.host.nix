ea142f9c0e9748286340ea0fa35ee076c3f8fd31
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-04-13 16:09:10 +0800

    Try xsession

--- home-manager/nixos/vbox-test.host.nix
+++ home-manager/nixos/vbox-test.host.nix
@@ -6,6 +6,15 @@
     # Empty as of now
   ];
 
+  xsession = {
+    enable = true;
+    numlock.enable = true;
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = false;
+    };
+  };
+
   xresources.extraConfig = ''
     XTerm*background: black
     XTerm*foreground: lightgray
--- vbox-test/overrides.nix
+++ vbox-test/overrides.nix
@@ -1,4 +1,17 @@
 { config, pkgs, lib, ... }:
 {
   networking.hostName = lib.mkForce "vbox-test";
+
+  services.xserver = {
+    enable = true;
+    displayManager = {
+      autoLogin.enable = lib.mkForce false;
+      session = [{
+        manage = "desktop";
+        name = "user_session";
+        desktopNames = [ "User-defined X11 Session" ];
+        start = ''exec $HOME/.xsession'';
+      }];
+    };
+  };
 }
