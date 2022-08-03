5fe2a2c0164e6c12c3d6bde9a0c5b1b9d7fd0c31
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-08-03 18:26:54 +0800

    Try force hostname

--- platform/wsl/base/configuration.nix
+++ platform/wsl/base/configuration.nix
@@ -15,6 +15,9 @@
     enable = true;
     automountPath = "/mnt";
     defaultUser = "nixos";
+    wslConf = {
+      network.hostname = "wslnix";
+    };
   };
 
   # WSL (regardless of 1 or 2) doesn't support X11, so it may help saving space
