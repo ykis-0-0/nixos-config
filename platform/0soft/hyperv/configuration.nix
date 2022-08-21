2cf95740e171bb1e07b59717044d2a6234a143c0
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-08-22 04:40:42 +0800

    Small format change

--- platform/0soft/hyperv/configuration.nix
+++ platform/0soft/hyperv/configuration.nix
@@ -1,5 +1,5 @@
-{ config, lib, pkgs, ... }:
-{
+{ config, lib, pkgs, ... }: {
+
   networking = {
     hostName = lib.mkDefault "hyperv";
     # The global useDHCP flag is deprecated, therefore explicitly set to false here.
