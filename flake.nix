c3b1f1644ba5c0abc907aaae51a193853c0059a6
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-08-23 02:03:15 +0800

    Separate homeConfigurations definitions from main file

--- flake.nix
+++ flake.nix
@@ -64,53 +64,8 @@
         };
       };
       mkHomeConfigurations = builders: builtins.listToAttrs (map mkHomeConfig' builders);
-    in mkHomeConfigurations [
-      {
-        username = "nixos";
-        host = "rpinix";
-        modules = [
-          ./home-manager/base.nix
-          ./home-manager/nixos/base.nix
-          ./home-manager/nixos/hosts/rpinix.nix
-          "${inputs.vscode-server-patch}/modules/vscode-server/home.nix"
-        ];
-        extraSpecialArgs = {
-          vscode-srv = inputs.vscode-server-patch;
-        };
-      }
-      {
-        username = "nixos";
-        host = "vbox-proxy";
-        modules = [
-          ./home-manager/base.nix
-          ./home-manager/nixos/base.nix
-          ./home-manager/nixos/hosts/vbox-proxy.nix
-        ];
-        extraSpecialArgs = {
-          inherit (inputs) nix-matlab;
-        };
-      }
-      {
-        username = "nixos";
-        host = "vbox-test";
-        modules = [
-          ./home-manager/base.nix
-          ./home-manager/nixos/base.nix
-          ./home-manager/nixos/hosts/vbox-test.nix
-        ];
-        extraSpecialArgs = {};
-      }
-      {
-        username = "nixos";
-        host = "wslnix";
-        modules = [
-          ./home-manager/base.nix
-          ./home-manager/nixos/base.nix
-          ./home-manager/nixos/hosts/wslnix.nix
-        ];
-        extraSpecialArgs = {};
-      }
-    ];
+      homeConfigurations' = import ./homes.nix inputs;
+    in mkHomeConfigurations homeConfigurations';
 
   };
 }
