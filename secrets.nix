cc7f05d5425fd9cc586dfe42ec2f88b8b26f277d
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-08-31 04:27:37 +0800

    A more reasonable secrets.nix I guess?

--- secrets.nix
+++ secrets.nix
@@ -1,1 +1,25 @@
-throw "Nononono, you're not peeking my secrets. Fuck off bitch :-)"
+let
+  REDACTED = usage: "Wanna steal my ${usage}? Next time bitch :-)";
+in {
+  wifi = {
+    "${REDACTED "WiFi SSID"}" = {
+      psk = REDACTED "WiFi password";
+      authProtocols = [ "WPA-PSK" ];
+      priority = 1;
+    };
+
+    "${REDACTED "WiFi SSID"}" = {
+      psk = REDACTED "WiFi password";
+      authProtocols = [ "WPA-PSK" ];
+    };
+  };
+
+  ddclient_noip = {
+    username = REDACTED "No-IP account";
+    passwordFile = builtins.toFile "ddclient-passwd" (REDACTED "No-IP password");
+    domains = [
+      # "ykis.ddns.net" # Allocated for minecraft server
+      "ykis-host.ddns.net"
+    ];
+  };
+}
