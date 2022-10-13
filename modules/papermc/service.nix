66d930fdf443ec00c62c3a692fe8bb8f656c5cde
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-10-14 04:37:07 +0800

    Split: purge non-base service

--- modules/papermc/service.nix
+++ modules/papermc/service.nix
@@ -1,83 +1,4 @@
 { config, lib, pkgs, ... }:{
-  options.services.papermc = let
-    inherit (lib) types mkOption mkEnableOption mkPackageOption;
-  in {
-    enable = mkEnableOption "the PaperMC Minecraft dedicated server";
-
-    # BUG unable to pipe outputs to systemd journal
-    # SEE ./scripts/bootstrapper.sh
-    # systemd-verbose = mkEnableOption "logging PaperMC console output to Systemd Journal (for debugging purposes only)";
-
-    port = mkOption {
-      type = types.port;
-      default = 25565;
-      description = "TCP & UDP port for PaperMC to bind against";
-    };
-
-    memory = let
-      mkMemOpt = valType: defVal: mkOption {
-        type = types.ints.positive;
-        default = defVal;
-        description = "${valType} size of the JVM Heap, in MiB";
-      };
-    in {
-      min = mkMemOpt "Minimum" 1024;
-      max = mkMemOpt "Maximum" 4096;
-    };
-
-    packages = {
-      jre = mkPackageOption pkgs "JRE" { default = [ "temurin-jre-bin" ]; };
-
-      papermc = {
-        version = mkOption {
-          type = types.str;
-          description = "Minecraft version to run PaperMC on";
-        };
-        build = mkOption {
-          type = types.nullOr types.ints.unsigned;
-          default = null;
-          description = "PaperMC build to pin against, or `null` to use latest";
-        };
-      };
-    };
-
-    storages = let
-      mkStorageOpt = key: type: mkOption {
-        type = types.path;
-        description = "Location of ${type} for PaperMC";
-      };
-    in builtins.mapAttrs mkStorageOpt {
-      # srv-root = "Server root directory"; # FIXME think again of whether really include it
-      bin = "binaries";
-      etc = "configuration files";
-      worlds = "world saves";
-      log = "logs";
-      plugins = "plugins";
-      cache = "binary cache";
-    };
-
-    cliArgs = {
-      jvm = mkOption {
-        type = types.lines;
-        default = "";
-        description = ''
-          Extra arguments to specify for the JVM when launching the server.\
-          `-Xmx` and `-Xms` should be set via `services.papermc.memory`.\
-          Multiple definitions will be merged in an unknown order.
-        '';
-      };
-
-      papermc = mkOption {
-        type = types.lines;
-        default = "";
-        description = ''
-          Extra arguments to specify for PaperMC itself when launching the server.\
-          Multiple definitions will be merged in an unknown order.
-        '';
-      };
-    };
-  };
-
   config = let
     selfCfg = config.services.papermc;
     papermc-scripts = pkgs.stdenvNoCC.mkDerivation {
@@ -97,8 +18,7 @@
     };
   in lib.mkIf selfCfg.enable {
     systemd = {
-      services = {
-        papermc = {
+      services.papermc = {
           enable = true;
           description = "PaperMC Minecraft dedicated server Instance";
 
@@ -160,29 +80,9 @@
             ExecStartPre = "${papermc-scripts}/updater.sh";
             ExecStart = "${papermc-scripts}/bootstrapper.sh launch /run/${RuntimeDirectory}/abduco.sock ${selfCfg.packages.jre}/bin/java @${argsFile}";
           };
-        };
-
-        # TODO Discord Webhook notifier service definition
-        discord-prophet = {};
-
-        # TODO BorgBase backup service definition
-        borgbase-mcbackup = {};
       };
 
       timers.sched-reboot.conflicts = [ "papermc.service" ];
     };
   };
-
-  /*
-    HACK Thing to be designed in regard to the  minecraft server infrastructure
-    - Main Server Instance
-      1. How should the connection between the binary and systemd be established? dtach / abduco?
-      2. Should the updater be put at ExecPreStart?
-    - BorgBase backup runner
-      1. How often should the runner be triggered?
-      2. And from which side (systemd / instance)?
-        a. If we use systemd, how should we instruct the instance to not touch the save / make a local snapshot?
-    - Discord Webhook postman
-      1. How should we trigger the webhook? (systemd / instance)?
-  */
 }
