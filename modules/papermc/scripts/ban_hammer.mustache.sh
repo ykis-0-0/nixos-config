3b55fc2d193534d784beb4912e11ddfdab4cd143
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-10-18 23:23:32 +0800

    Fix: create substitution of abduco

--- modules/papermc/scripts/ban_hammer.mustache.sh
+++ modules/papermc/scripts/ban_hammer.mustache.sh
@@ -2,6 +2,7 @@
 # shellcheck shell=sh
 
 PWAIT_EXE="{{extrace}}/bin/pwait"
+ABDUCO_EXE="{{abduco}}/bin/abduco"
 
 do_pwait() {
   "$PWAIT_EXE" "$2"
@@ -9,7 +10,7 @@ do_pwait() {
 }
 
 echo "[Ban Hammer] Commanding PaperMC to /stop"
-echo 'stop' | abduco -p "${RUNTIME_DIRECTORY}/abduco.sock"
+echo 'stop' | "$ABDUCO_EXE" -p "${RUNTIME_DIRECTORY}/abduco.sock"
 
 echo "[Ban Hammer] Waiting for server processes to exit"
 do_pwait "PaperMC" "$(cat "${RUNTIME_DIRECTORY}/jvm.pid")"&
