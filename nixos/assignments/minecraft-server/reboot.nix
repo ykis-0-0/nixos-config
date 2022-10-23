b2707272c5be10ea40564be64a991441052d2d7d
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2022-10-23 22:11:42 +0800

    Decrease reboot frequency

--- nixos/assignments/minecraft-server/reboot.nix
+++ nixos/assignments/minecraft-server/reboot.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }: {
   services.sched-reboot = {
     enabled = true;
-    happenOn = "*-*-* 04:00:00";
+    happenOn = "*-*-1..31/2 04:00:00";
     gracePeriod = "3min";
   };
 }
