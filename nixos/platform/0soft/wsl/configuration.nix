4e92fef6bc8efa2934f5e478797ac35ba42d172f
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2023-01-06 23:39:57 +0800

    WSLg! Hooray!!!

--- nixos/platform/0soft/wsl/configuration.nix
+++ nixos/platform/0soft/wsl/configuration.nix
@@ -21,9 +21,6 @@
     docker-desktop.enable = true;
   };
 
-  # WSL (regardless of 1 or 2) doesn't support X11, so it may help saving space
-  environment.noXlibs = true;
-
   users.users.root.hashedPassword = null;
 
   programs.ssh.startAgent = true;
