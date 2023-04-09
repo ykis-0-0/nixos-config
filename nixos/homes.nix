5aed5c4376c3d06aa9700f6056a76e6ac355c063
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2023-04-09 15:49:14 +0800

    Imean... how long since we last used rpinix by itself?

--- nixos/homes.nix
+++ nixos/homes.nix
@@ -1,17 +1,6 @@
 inputs: [
   {
     username = "nixos";
-    host = "rpinix";
-    modules = [
-      ./home-manager/base.nix
-      ./home-manager/nixos/base.nix
-      ./home-manager/nixos/hosts/rpinix.nix
-      inputs.vscode-server-patch.nixosModules.home
-    ];
-    extraSpecialArgs = {};
-  }
-  {
-    username = "nixos";
     host = "wslnix";
     modules = [
       ./home-manager/base.nix
