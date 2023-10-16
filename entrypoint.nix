eb102db1785f99e0f8f6c925abbedd194ee3b4d8
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2023-10-17 02:58:43 +0800

    Offload inputs back to `flake.nix`

--- entrypoint.nix
+++ entrypoint.nix
@@ -1,57 +1,4 @@
-{
-  description = "System Configuration(s)";
-
-  inputs = {
-    # region (Semi-)Endorsed Modules
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL/main";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    impermanence.url = "github:nix-community/impermanence";
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-      # flake = false;
-    };
-    vscode-server-patch = {
-      url = "github:nix-community/nixos-vscode-server/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    # endregion
-    # region Thrid-party Modules
-    /* # We aren't using MATLAB anytime soon i guess (probably forever lmao)
-    nix-matlab = {
-      url = "gitlab:doronbehar/nix-matlab/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    */
-    deploy-rs = {
-      url = "github:serokell/deploy-rs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    manix = {
-      url = "github:kulabun/manix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
-    # endregion
-    # region Homebrew
-    argononed = {
-      url = "gitlab:DarkElvenAngel/argononed/master";
-      flake = false;
-    };
-    npiperelay = {
-      url = "github:ykis-0-0/npiperelay.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    # secret-wrapper: to be supplied on target hosts
-    secret-wrapper.follows = "";
-    # endregion
-  };
-
-  outputs = { self, secret-wrapper ? null, ... }@inputs': let
+{ self, secret-wrapper ? null, ... }@inputs': let
     #region sched-reboot injection
     # HACK revert to normal flake input definition whether possible,
     # specifically when https://github.com/NixOS/nix/issues/6352 has merged
@@ -106,5 +53,4 @@
       nodes = import ./nixos/deployments.nix inputs;
       remoteBuild = true;
     };
-  };
-}
+  }
---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "System Configuration(s)";
+
+  inputs = {
+    # region (Semi-)Endorsed Modules
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impermanence.url = "github:nix-community/impermanence";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+      # flake = false;
+    };
+    vscode-server-patch = {
+      url = "github:nix-community/nixos-vscode-server/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # endregion
+    # region Thrid-party Modules
+    /* # We aren't using MATLAB anytime soon i guess (probably forever lmao)
+    nix-matlab = {
+      url = "gitlab:doronbehar/nix-matlab/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    */
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    manix = {
+      url = "github:kulabun/manix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+    # endregion
+    # region Homebrew
+    argononed = {
+      url = "gitlab:DarkElvenAngel/argononed/master";
+      flake = false;
+    };
+    npiperelay = {
+      url = "github:ykis-0-0/npiperelay.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # secret-wrapper: to be supplied on target hosts
+    secret-wrapper.follows = "";
+    # endregion
+  };
+
+  outputs = inputs: import ./entrypoint.nix inputs;
+}
