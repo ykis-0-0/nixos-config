84bf3db86d9539fa815a1e7e9cffc4e4e6bc4511
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2023-11-13 00:13:03 +0800

    Add check for home-manager's configs
    Closes #4

--- entrypoint.nix
+++ entrypoint.nix
@@ -49,6 +49,18 @@ in {
     );
   in mkHomeConfigurations homeConfigurations';
 
+  checks = let
+    mapAttrsToList = let # Thanks https://github.com/NixOS/nixpkgs/blob/master/lib/attrsets.nix#L518 <3
+      inherit (builtins) map attrNames;
+      applier = fn: attrs: key: fn key attrs.${key};
+    in
+      fn: attrs: map (applier fn attrs) (attrNames attrs);
+    nameValuePair = name: value: { inherit name value; };
+
+    hmCfgPerSys = builtins.groupBy (nvp: nvp.value.pkgs.hostPlatform.system) (mapAttrsToList (nameValuePair) self.homeConfigurations);
+    hmChecks = builtins.mapAttrs (_: attrs: builtins.mapAttrs (_: cfg: cfg.activationPackage) (builtins.listToAttrs attrs)) hmCfgPerSys;
+  in hmChecks // {};
+
   deploy = {
     nodes = import ./deployments.nix inputs;
     remoteBuild = true;
